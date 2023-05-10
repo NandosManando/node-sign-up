f9dd975ea175ff3466aee99936a3414853eb1ee5
NandosManando <127309611+NandosManando@users.noreply.github.com>
2023-05-10 10:51:09 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <form className="wrapper">
+            <h2>Sign Up</h2>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Name Of Company
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Name Of Employer
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Position
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Location
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Salary
+                </label>
+            </section>
+            
+            <button type="button" className="btn">
+                SUBMI
+            </button>
+            <span className="footer"></span>
+        </form>
+  );
+}
+
+
+
+export default App;
